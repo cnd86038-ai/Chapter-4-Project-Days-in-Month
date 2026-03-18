14761b950863966345d6a9371039d894b9d6ce0b
cnd86038-ai <cnd86038@email.vccs.edu>
2026-03-18 15:14:45 -0400

    Input and Output for month and  year.

--- Chapter 4 Project Report Days in Month/main.cpp
+++ Chapter 4 Project Report Days in Month/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+	int month, year;
+	int daysInMonth; // Variable to store the number of days in the month
+	bool leapYear; // Variable to determine if the year is a leap year
 
+	// Prompt the user for input month
+	cout << "Enter month (1-12): ";
+	cin >> month;
+	// Prompt the user for input year
+	cout << "Enter year: ";
+	cin >> year;
 
 }
